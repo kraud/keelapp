406a129746b70b450f001f2de1f5ae65d092e3a4
Kevin Raud <kevinchos@gmail.com>
2023-09-29 20:54:14 -0300

    Adding tags to account-screen. Working on UI for future tag-sharing?.

--- frontend/src/pages/UserData.tsx
+++ frontend/src/pages/UserData.tsx
@@ -1,19 +1,38 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {motion} from "framer-motion";
 import {routeVariantsAnimation} from "./management/RoutesWithAnimation";
 import globalTheme from "../theme/theme";
-import {Button, Grid, Typography} from "@mui/material";
-import {useSelector} from "react-redux";
+import {Button, Chip, Grid, Typography} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
 import Avatar from "@mui/material/Avatar";
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import SettingsIcon from '@mui/icons-material/Settings';
+import {searchAllTags} from "../features/words/wordSlice";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import LinearIndeterminate from "../components/Spinner";
 
 interface UserDataProps {
 
 }
 
 export const UserData = (props: UserDataProps) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    let  [searchParams, setSearchParams]  = useSearchParams();
     const {user} = useSelector((state: any) => state.auth)
+    const {searchResults, isSearchLoading} = useSelector((state: any) => state.words)
+    const [allTags, setAllTags] = useState<string[]>([])
 
     const friendList = ["friend1", "friend2", "friend3", "friend4","friend5", "friend6"]
+
+    useEffect(() => {
+        setAllTags(searchResults)
+    },[searchResults])
+
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(searchAllTags(""))
+    },[])
 
     return(
         <Grid
@@ -120,36 +139,94 @@ export const UserData = (props: UserDataProps) => {
             </Grid>
             <Grid
                 container={true}
-                justifyContent={"space-between"}
+                justifyContent={"space-around"}
                 item={true}
                 xs={12}
-                spacing={2}
             >
                 <Grid
                     item={true}
-                    xs={6}
+                    xs={5}
                 >
                     <Button
                         variant={"contained"}
                         color={"primary"}
                         onClick={() => null}
                         fullWidth={true}
+                        startIcon={<PersonAddIcon />}
                     >
                         Add friends
                     </Button>
                 </Grid>
                 <Grid
                     item={true}
-                    xs={6}
+                    xs={5}
                 >
                     <Button
                         variant={"contained"}
                         color={"secondary"}
                         onClick={() => null}
                         fullWidth={true}
+                        endIcon={<SettingsIcon />}
                     >
                         Edit profile
                     </Button>
+                </Grid>
+            </Grid>
+            <Grid
+                container={true}
+                justifyContent={"center"}
+                item={true}
+                xs={12}
+                spacing={2}
+            >
+                <Grid
+                    item={true}
+                >
+                    <Typography
+                        sx={{
+                            typography: {
+                                xs: 'h3',
+                                sm: 'h2',
+                                md: 'h1',
+                            },
+                            textTransform: "uppercase",
+                            textDecoration: "underline"
+                        }}
+                    >
+                        Tags:
+                    </Typography>
+                </Grid>
+                <Grid
+                    item={true}
+                    container={true}
+                >
+                    {(isSearchLoading)
+                        ?
+                        <LinearIndeterminate/>
+                        :
+                        (allTags.map((tag: string, index: number) => {
+                            return(
+                                <Grid
+                                    item={true}
+                                    key={index}
+                                >
+                                    <Chip
+                                        variant="filled"
+                                        label={tag}
+                                        color={"secondary"}
+                                        sx={{
+                                            maxWidth: "max-content",
+                                        }}
+                                        onClick={() => {
+                                            // TODO: temporary function? It should open modal with list of words
+                                            //  + options to hide the tag from friends, send tag to friend, etc?
+                                            navigate('/review/tags?tags='+tag)
+                                        }}
+                                    />
+                                </Grid>
+                            )
+                        }))
+                    }
                 </Grid>
             </Grid>
             <Grid
@@ -184,21 +261,40 @@ export const UserData = (props: UserDataProps) => {
                         (friendList.map((friend: string, index: number) => {
                             return(
                                 <Grid
+                                    container={true}
                                     item={true}
                                     xs={12}
                                     key={index}
+                                    sx={{
+                                        background: (index % 2 === 0) ?"#c7c7c7" :undefined,
+                                        paddingY: globalTheme.spacing(1),
+                                        borderBottom: (index === friendList.length -1) ?'1px solid black' :"none",
+                                    }}
                                 >
-                                    <Typography
+                                    <Grid
+                                        item={true}
                                         sx={{
-                                            typography: {
-                                                xs: 'h5',
-                                                sm: 'h4',
-                                                md: 'h3',
-                                            },
+                                            borderRight: '1px solid black',
                                         }}
                                     >
-                                        {friend}
-                                    </Typography>
+                                        <Typography
+                                            sx={{
+                                                typography: {
+                                                    xs: 'h5',
+                                                    sm: 'h4',
+                                                    md: 'h3',
+                                                },
+                                                textTransform: "capitalize"
+                                            }}
+                                        >
+                                            {friend}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid
+                                        item={true}
+                                    >
+
+                                    </Grid>
                                 </Grid>
                             )
                         }))
