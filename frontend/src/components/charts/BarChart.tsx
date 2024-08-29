96f2fbab9a39e976bca65f3dfcb8898385307af0
MatiBlanco <blanco.mati1@gmail.com>
2024-08-28 23:52:44 -0300

    Added some comments

--- frontend/src/components/charts/BarChart.tsx
+++ frontend/src/components/charts/BarChart.tsx
@@ -28,26 +28,40 @@ const parseData = (dataArray: WordsPerMonth []): any => {
     const arrayKeys : any [] = []
     if(dataArray.length > 0) {
         dataArray.forEach((element: WordsPerMonth) => {
+            //We get the index where the label being processed is.
             const index = arrayData.findIndex((timeSlap) => timeSlap.name.includes(element.label))
             if (index >0) {
+                //If the array have the label being processed, it will only add the new partOfSpech data.
                 arrayData[index] = {
                     ...arrayData[index],
                     [element._id.partOfSpeech]: element.count
                 }
             } else {
+                //If the array doesnt have the label being processed, it will add it.
                 arrayData.push({
                     "name": element.label,
                     [element._id.partOfSpeech]: element.count
                 })
             }
-
+            //We verify if the label had been processed.
             const keys = arrayKeys.find((key) => key.includes(element._id.partOfSpeech))
             if(keys === undefined){
+                //if the label doesnt exist in array, we will add the label to the array.
                 arrayKeys.push(element._id.partOfSpeech)
             }
         })
     }
     return {
+        /*
+        example
+        json: [
+                {name: '2023-01', verbs: 200, nouns: 200},
+                {name: '2023-02', verbs: 100, nouns: 300},
+                {name: '2023-04', verbs: 300, nouns: 200},
+                {name: '2023-05', verbs: 400, nouns: 100},
+            ]
+
+         */
          json: arrayData,
             keys: {
                 x: 'name',
