31acfb43b676c516bb5f8569e33c1e691f5eb3c1
MatiBlanco <blanco.mati1@gmail.com>
2024-10-04 12:56:23 -0300

    Added new html to generate a email templates

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer")
+const fs = require('fs');
+const path = require('path');
+const {string} = require("yup");
 
-module.exports = async(email, subject, text) => {
+module.exports = async(emailData) => {
     const transporter =  nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         service: process.env.EMAIL_SERVICE,
@@ -12,6 +15,12 @@ module.exports = async(email, subject, text) => {
         }
 
     })
+    const mailData = {
+        from: process.env.EMAIL_USER,
+        to: emailData.email,
+        subject: emailData.subject
+    }
+
     await new Promise((resolve, reject) => {
         // verify connection configuration
         transporter.verify(function (error, success) {
@@ -24,22 +33,32 @@ module.exports = async(email, subject, text) => {
         })
     })
 
-    const mailData = {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: subject,
-        text: text
-    }
-
-    await new Promise((resolve, reject) => {
-        // send mail
-        transporter.sendMail(mailData, (err, info) => {
+    async function generateHtmlAndSend(emailData) {
+        const filePath = path.join(__dirname, '/resources/');
+        await fs.readFile(filePath + emailData.type + '.html', 'utf8', (err, html) => {
             if (err) {
-                console.error(err)
-                reject(err)
-            } else {
-                resolve(info)
+                console.log('Error loading template:', err)
+                return ""
             }
+
+            // Replace variables in the template
+            mailData.html = html
+                .replace('{{replace-url}}', emailData.url)
+                .replace('{{replace-name}}', emailData.name)
+                .replace('{{replace-resources}}', filePath)
+
+            new Promise(async (resolve, reject) => {
+                await transporter.sendMail(mailData, (err, info) => {
+                    if (err) {
+                        console.error(err)
+                        reject(err)
+                    } else {
+                        resolve(info)
+                    }
+                })
+            })
+
         })
-    })
+    }
+    await generateHtmlAndSend(emailData);
 }
