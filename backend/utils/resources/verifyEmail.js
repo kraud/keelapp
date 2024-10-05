8b1c2ca7d5329342c03c1a3bec2c75dae0f61b1a
MatiBlanco <blanco.mati1@gmail.com>
2024-10-05 12:11:50 -0300

    Test in Vercel

--- backend/utils/resources/verifyEmail.js
+++ backend/utils/resources/verifyEmail.js
@@ -4,7 +4,7 @@ const verifyEmailHtmlComponent = (name, url, email) =>
 {
     const myHtl = `
      <div>
-        <img src="cid:logo" alt="Logo" style="max-width: 180px;"/>
+<!--        <img src="cid:logo" alt="Logo" style="max-width: 180px;"/>-->
         <h1>Hello ${name}</h1>
         <p>Once you have confirmed that ${email} is your email address, you will be able to use our platform.</p>
         <p>Click on the following link to verify your email: <a href="${url}"> here!</a></p>
