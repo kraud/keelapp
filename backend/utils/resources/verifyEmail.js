6518821982276762ea6561fa1b6bb8f65b772929
MatiBlanco <blanco.mati1@gmail.com>
2024-10-05 23:42:45 -0300

    Added logs to verify in Vercel V2

--- backend/utils/resources/verifyEmail.js
+++ backend/utils/resources/verifyEmail.js
@@ -2,16 +2,13 @@ const path = require('path');
 
 const verifyEmailHtmlComponent = (name, url, email) =>
 {
-    const myHtl = `
-     <div>
+    return `<div>
         <img src="cid:logo" alt="Logo" style="max-width: 180px;"/>
         <h1>Hello ${name}</h1>
         <p>Once you have confirmed that ${email} is your email address, you will be able to use our platform.</p>
         <p>Click on the following link to verify your email: <a href="${url}"> here!</a></p>
         <p>Thank you!</p>
-    </div>
-    `;
-    return myHtl;
+    </div>`;
 };
 
 const getAttachmentsVerifyEmail = () => {
